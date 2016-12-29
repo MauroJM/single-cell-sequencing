dbfbde69c1941aa48cb21308277d3bfe679b28cc
Mauro <muraro.mauro@gmail.com>
2016-12-29 15:06:59 +0100

    Update plate_diagnostics.R

--- plate_diagnostics.R
+++ plate_diagnostics.R
@@ -1,3 +1,14 @@
+# Script for making a pdf containing diagnostic plots of SORT-Seq data. It goes trough the following steps:
+# optional: transform CS1 style data to CS2-style layout (from 4 96-cell libraries to 1 384-cell library)
+# read in ALL .cout* files in one folder
+# lable cells with unique name taken from the input .cout* files and a number for each cell
+# optional: rename part of specific plates in the case of two different experiments in one plate
+# plot one pdf per plate containing diagnostic plots
+# merge specified plates into one file that can be loaded into RaceID
+
+## questions can be addressed to m.muraro@hubrecht.eu
+
+####install/load packages and variables####
 # install.packages("oce") # install this package upon first use
 source("~/AvO_lab/R/scripts/plate_diagnostics_functions2.R") # specify path to functions file
 require(RColorBrewer)
@@ -14,7 +25,7 @@ tc<-list() # list of .coutt files
 rc<-list() # list of .coutc files
 bc<-list() # list of .coutb files
 
-####Merge CS1-type libraries into one 384-column long CS2-type file####
+#### OPTIONAL: Merge CS1-type libraries into one 384-column long CS2-type file####
 # do this ONLY if you have CS1-style (4 libraries in one plate) data that you want to run this script on
 # this assumes the order of A1=lib1, A2=lib2, B1=lib3, B2=lib4 when it reorders the columns
 # setwd("/Users/mauro/AvO_lab/R/GK_GateID/raw_files/GK2E_GK2F/") # set directory containing the files
@@ -22,7 +33,7 @@ bc<-list() # list of .coutb files
 # input required is 4 unique library handles in the right order, followed by the name of the new, merged file, like so:
 # reorder.cs1(c("EP10-12-1_AHVK72BGXX_S1","EP10-12-2_AHVK72BGXX_S2","EP10-12-3_AHVK72BGXX_S3","EP10-12-4_AHVK72BGXX_S4"),"EP10-CS2-live")
 
-####read files ####
+####read files and lable cells ####
 #read data into four lists
 files <- read_files(dir = getwd()) # path to files with .cout* extention. NB: ALL files in the dir will be processed
 
@@ -41,7 +52,12 @@ for(i in 1:length(files)){
 #lable cells: all cells in library will get a _1 to _384 extension to the library name specified in names object
 for(i in 1:length(tc)){ colnames(tc[[i]])<-paste(names[[i]],c(1:384),sep="_") }
 
-#### diagnostic plot s####
+# OPTIONAL: rename part of a specified plate if you have different experiments in one plate
+# do this only for one of the .coutt files at the time by choosing the correct location in the .coutt list (eg:tc[[5]])
+# and manually specify which cells(eg: 1:384) are renamed to what (eg:"alpha"/"beta")
+names(tc[[5]])<-c(paste("alpha",c(1:192),sep="_"),paste("beta",c(1:192),sep="_")) 
+
+#### diagnostic plots####
 path<-"/Users/mauro/Desktop/diagnostic plots/" # specify where you want the diagnostic plots
 dir.create(path, showWarnings = F) # directory will be made if it doesn't exist
 setwd(path)
@@ -60,12 +76,7 @@ for(i in 1:length(tc)){
   dev.off()
 } #make pdf with diagnostic plots
 
-# rename part of the plates if you have different experiments in one plate
-# do this only for one of the .coutt files at the time by choosing the location in the .coutt list
-# and manually specify which cells are renamed to what
-names(tc[[5]])<-c(paste("alpha",c(1:192),sep="_"),paste("beta",c(1:192),sep="_")) 
-
-#merge all dataframes of object into one and write a .csv
+####merge and write multiple dataframes into one .csv####
 cdata_all<-tc[[1]] # should be first position you want to start merging from
 for (i in 2:length(tc)){
   cdata_all <- intersectmatrix(cdata_all,tc[[i]]) #  JC's function intersectmatrix
